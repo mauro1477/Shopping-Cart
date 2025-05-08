b40884a6be4113d7aba2c29ebb21c25eaa054f8b
mauro <maurovargas7725@gmail.com>
2025-05-07 21:55:21 -0600

    Added Firebase auth to Detail Page

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -8,6 +8,23 @@ import ProductsPage from './pages/ProductsPage.vue';
 import ProductDetailPage from './pages/ProductDetailPage.vue';
 import PageNotFound from './pages/PageNotFound.vue';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: "AIzaSyCArTo7YahvT-zewRPK3l7SvyL9jznHJ-k",
+authDomain: "shopping-cart-44486.firebaseapp.com",
+projectId: "shopping-cart-44486",
+storageBucket: "shopping-cart-44486.firebasestorage.app",
+messagingSenderId: "814166916282",
+appId: "1:814166916282:web:c8df565bc630350d9a1683"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 createApp(App)
 .use(VueRouter.createRouter({
     history: VueRouter.createWebHistory(import.meta.env.VITE_API_BASE_URL),
--- front-end/src/pages/ProductDetailPage.vue
+++ front-end/src/pages/ProductDetailPage.vue
@@ -8,7 +8,7 @@
 			<h3 class="price">{{ product.price }}</h3>
 			<button @click="addToCart" class="add-to-cart" v-if="!isItemInCart">Add to Cart</button>
 			<button class="grey-button" v-else>Item is already in cart</button>
-
+			<button @click="signIn" class="sign-in">Sign in to add to cart</button>
 		</div>
 	</div>
 	<div v-else>
@@ -17,6 +17,8 @@
 </template>
 
 <script>
+import { getAuth, sendSignInLinkToEmail, isSignInWithEmailLink, signInWithEmailLink } from "firebase/auth";
+
 import axios from 'axios';
 import PageNotFound from './PageNotFound.vue';
 export default{
@@ -39,9 +41,29 @@ export default{
     async addToCart(){
 		await axios.post('/api/users/1234/cart',{ id : this.$route.params.productId });
 		alert('Successfully added item to cart!');
+	},
+	async signIn(){
+		const email = prompt('Please enter your email to sign in:');
+		const auth = getAuth();
+		const actionCodeSettings = {
+			url: `http://localhost:5173/products/${this.$route.params.productId}`,
+			handleCodeInApp: true
+		}
+		await sendSignInLinkToEmail(auth, email, actionCodeSettings);
+		alert('A login link was sent to the email you provided');
+		window.localStorage.setItem('emailForSignIn', email);
 	}
   },
   async created() {
+	//Make sure user got to Details Page with auth link
+	const auth = getAuth();
+	if(isSignInWithEmailLink(auth,window.location.href)){
+		const email = window.localStorage.getItem('emailForSignIn');
+		await signInWithEmailLink(auth, email, window.location.href);
+		alert('Successfully signed in!');
+		window.localStorage.removeItem('emailForSignIn');
+	}
+
 	const response = await axios.get(`/api/products/${this.$route.params.productId}`);
 	const product = response.data;
 	this.product = product;
